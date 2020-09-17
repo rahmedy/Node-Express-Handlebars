325b09d0a6e26960aa1a01145d515ff41eabc2ab
Rayan Ahmedy <rayanahmedy@Rayans-MacBook-Pro.local>
2020-09-17 02:22:25 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
+const express = require('express');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-// For POST and PUT
-app.use(express.urlencoded({ extended: true }));
+// Parse application body as JSON
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
 app.use(express.json());
 
 // Set Handlebars.
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+const exphbs = require('express-handlebars');
+app.engine( 'handlebars',exphbs({defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them.
+const routes = require('./controller/burgers_controller.js');
+app.use(routes);
+
+// Start server
+app.listen(PORT, function () {
+    console.log('Server is listening on: http://localhost:' + PORT);
+});
